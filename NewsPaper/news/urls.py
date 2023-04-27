b9106ef3e77f74dd2b2333b8aa9f39d9ed12a2fa
fonrabe <yura.fon-rabe@mail.ru>
2023-04-27 06:48:55 +0300

    Add filters

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from news.views import PostsList, PostDetails
+from news.views import PostsList, PostDetails, CreateNews, PostSearch
 
 urlpatterns = [
     path('', PostsList.as_view(), name = 'post_list'),
     path('<int:pk>', PostDetails.as_view()),
-    # path('create/', CreateNews.as_view(), name = 'add_news'),
+    path('search/', PostSearch.as_view()),
+    path('create/', CreateNews.as_view(), name = 'add_news'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView, DetailView, CreateView
+from .filters import PostFilter
 from news.models import Post
 
 
@@ -16,3 +16,25 @@ class PostDetails(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
+
+
+class CreateNews(CreateView):
+    pass
+
+
+class PostSearch(ListView):
+    model = Post
+    ordering = '-dateCreate'
+    template_name = 'posts_search.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
